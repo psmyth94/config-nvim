55151621c0f9f8e39f4530bcd08418f795977e90
psmyth94 <psmyth1994@gmail.com>
2024-05-24 11:16:40 -0500

    move mappings

--- lua/psmyth/remap.lua
+++ lua/psmyth/remap.lua
@@ -26,7 +26,7 @@ map("i", "<C-c>", "<Esc>")
 map("n", "Q", "<nop>")
 map("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 map("n", "<leader><leader>", vim.lsp.buf.format)
-map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("n", "<leader>s", [[:%s/\v<<C-r><C-w>>//gI<Left><Left><Left>]])
 
 -- for goml and nvim quick access
 map("n", "<leader>nv", "<cmd>Ex ~/.config/nvim<CR>")
@@ -127,3 +127,89 @@ end)
 
 -- Fugitive
 map('n', '<leader>gp', '<cmd>Git push origin<CR>', opts)
+
+-- Harpoon
+vim.keymap.set("n", "<leader>a", require("harpoon.mark").add_file)
+vim.keymap.set("n", "<C-e>", require("harpoon.ui").toggle_quick_menu)
+
+vim.keymap.set("n", "<C-h>", function() require("harpoon.ui").nav_file(1) end)
+vim.keymap.set("n", "<C-t>", function() require("harpoon.ui").nav_file(2) end)
+vim.keymap.set("n", "<C-n>", function() require("harpoon.ui").nav_file(3) end)
+vim.keymap.set("n", "<C-s>", function() require("harpoon.ui").nav_file(4) end)
+vim.keymap.set("n", "<leader><C-h>", function() require("harpoon.mark").set_current_at(1) end)
+vim.keymap.set("n", "<leader><C-t>", function() require("harpoon.mark").set_current_at(2) end)
+vim.keymap.set("n", "<leader><C-n>", function() require("harpoon.mark").set_current_at(3) end)
+vim.keymap.set("n", "<leader><C-s>", function() require("harpoon.mark").set_current_at(4) end)
+
+-- Slime
+-- some ipython specific mappings
+local function slime_send(send_to_repl)
+    local start_line = vim.fn.search('# %%', 'bnW')
+
+
+    if start_line == 0 then
+        start_line = 1
+    end
+
+
+    local end_line = vim.fn.search('# %%', 'nW')
+    if end_line == 0 then
+        end_line = vim.fn.line('$')
+    else
+        end_line = end_line - 1
+    end
+
+
+    if send_to_repl then
+        vim.fn['slime#send_range'](start_line, end_line)
+    end
+
+    return end_line
+end
+
+vim.keymap.set('n', '<leader>cc', function()
+    slime_send(true)
+end, { noremap = true, silent = true })
+
+
+vim.keymap.set('n', '<C-X>', function()
+    local end_line = slime_send(true)
+
+    if vim.fn.search('# %%', 'nW') == 0 then
+        vim.cmd('$')
+        vim.cmd('normal o# %%')
+    else
+        vim.cmd([[normal ]] .. end_line + 2 .. 'G')
+    end
+end, { noremap = true, silent = true })
+
+
+vim.keymap.set('n', '<leader>nc', function()
+    local end_line = slime_send(false)
+
+    if vim.fn.search('# %%', 'nW') == 0 then
+        vim.cmd('$')
+        vim.cmd('normal o')
+        vim.cmd('normal o# %%')
+    else
+        vim.cmd([[normal ]] .. end_line .. 'GO')
+        vim.cmd([[normal O# %%]])
+    end
+
+    vim.cmd('normal j')
+    vim.cmd('startinsert')
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>rr', function()
+    vim.fn['slime#send']('exit()\n')
+    vim.fn['slime#send']('ipython\n')
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>ip', function()
+    vim.fn['slime#send']('ipython\n')
+end, { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>gm', function()
+    --
+    vim.fn['slime#send']('conda activate genomicsml\n')
+end, { noremap = true, silent = true })
