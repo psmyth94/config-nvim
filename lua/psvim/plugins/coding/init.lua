3ffa7d337a4a0b898d5efd2bf332201a098797ff
psmyth94 <psmyth1994@gmail.com>
2025-02-15 19:37:21 -0500

    refactor: update Copilot and coding plugin configurations
    - Replaced 'github/copilot.vim' with 'zbirenbaum/copilot.lua'.
    - Added configurations for Copilot suggestions and lualine status.
    - Updated coding plugin configurations for consistency.
    - Improved text-objects and auto pairs settings.

--- lua/psvim/plugins/coding/init.lua
+++ lua/psvim/plugins/coding/init.lua
@@ -1,14 +1,14 @@
 return {
   -- auto pairs
   {
-    "echasnovski/mini.pairs",
-    event = "VeryLazy",
+    'echasnovski/mini.pairs',
+    event = 'VeryLazy',
     opts = {
       modes = { insert = true, command = true, terminal = false },
       -- skip autopair when next character is one of these
       skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
       -- skip autopair when the cursor is inside these treesitter nodes
-      skip_ts = { "string" },
+      skip_ts = { 'string' },
       -- skip autopair when next character is closing pair
       -- and there are more closing pairs than opening pairs
       skip_unbalanced = true,
@@ -22,41 +22,41 @@ return {
 
   -- comments
   {
-    "folke/ts-comments.nvim",
-    event = "VeryLazy",
+    'folke/ts-comments.nvim',
+    event = 'VeryLazy',
     opts = {},
   },
 
   -- Better text-objects
   {
-    "echasnovski/mini.ai",
-    event = "VeryLazy",
+    'echasnovski/mini.ai',
+    event = 'VeryLazy',
     opts = function()
-      local ai = require("mini.ai")
+      local ai = require 'mini.ai'
       return {
         n_lines = 500,
         custom_textobjects = {
-          o = ai.gen_spec.treesitter({ -- code block
-            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-          }),
-          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
-          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
-          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
-          d = { "%f[%d]%d+" }, -- digits
+          o = ai.gen_spec.treesitter { -- code block
+            a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+            i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+          },
+          f = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' }, -- function
+          c = ai.gen_spec.treesitter { a = '@class.outer', i = '@class.inner' }, -- class
+          t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' }, -- tags
+          d = { '%f[%d]%d+' }, -- digits
           e = { -- Word with case
-            { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
-            "^().*()$",
+            { '%u[%l%d]+%f[^%l%d]', '%f[%S][%l%d]+%f[^%l%d]', '%f[%P][%l%d]+%f[^%l%d]', '^[%l%d]+%f[^%l%d]' },
+            '^().*()$',
           },
           g = PSVim.mini.ai_buffer, -- buffer
           u = ai.gen_spec.function_call(), -- u for "Usage"
-          U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+          U = ai.gen_spec.function_call { name_pattern = '[%w_]' }, -- without dot in function name
         },
       }
     end,
     config = function(_, opts)
-      require("mini.ai").setup(opts)
-      PSVim.on_load("which-key.nvim", function()
+      require('mini.ai').setup(opts)
+      PSVim.on_load('which-key.nvim', function()
         vim.schedule(function()
           PSVim.mini.ai_whichkey(opts)
         end)
@@ -65,15 +65,15 @@ return {
   },
 
   {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    cmd = "LazyDev",
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    cmd = 'LazyDev',
     opts = {
       library = {
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        { path = "psvim", words = { "psvim" } },
-        { path = "snacks.nvim", words = { "Snacks" } },
-        { path = "lazy.nvim", words = { "psvim" } },
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+        { path = 'psvim', words = { 'psvim' } },
+        { path = 'snacks.nvim', words = { 'Snacks' } },
+        { path = 'lazy.nvim', words = { 'psvim' } },
       },
     },
   },
